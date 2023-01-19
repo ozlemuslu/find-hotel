f591a8e7603eeb05e37b5615091a304ff8b92e86
ozlemuslu <ozlemusslu@gmail.com>
2023-01-19 16:48:26 +0300

    added routes, i18m, created pages

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import FindHotels from '../views/FindHotels.vue';
+import AddHotels from '../views/AddHotels.vue';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/hotels',
+    name: 'FindHotels',
+    component: FindHotels,
+  },
+  {
+    path: '/add-hotels',
+    name: 'AddHotels',
+    component: AddHotels,
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
---
+++ src/views/FindHotels.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <card />
+  </div>
+</template>
+
+<script>
+import Card from "../components/HotelCard.vue";
+
+export default {
+  name: "FindHotels",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style></style>

465fda81d1a3937395f07bce7f5602bc6970d277
ozlemuslu <ozlemusslu@gmail.com>
2023-01-22 12:48:16 +0300

    completed add hotel page

--- src/components/HotelCard.vue
+++ src/components/HotelCard.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="card" style="width: 18rem; border: 1px solid red">
-    <div class="card-body">
-      <h5 class="card-title">hey</h5>
-      <div>hey</div>
-      <div class="d-flex">
-        <button>Arttır</button>
-        <button>Azalt</button>
+  <div class="row">
+    <div class="col-12 col-lg-4">
+      <img src="../assets/hotelImage.png" alt="" />
+    </div>
+    <div class="col-12 col-lg-8 pl-0">
+      <div class="card" style="border: 1px solid red">
+        <div class="card-body">
+          <h5 class="card-title">hey</h5>
+          <div>hey</div>
+          <div class="d-flex">
+            <button>Arttır</button>
+            <button>Azalt</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
